312b85b57d9edf27a226b18f4dc2354d79bd92cd
RohanSubba <rohan.ronaldo07.rs@gmail.com>
2021-03-19 02:19:36 +0530

    completed

--- app/build/generated/source/buildConfig/debug/me/recycler/recipesrecyclerview/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/me/recycler/recipesrecyclerview/BuildConfig.java
@@ -5,7 +5,7 @@ package me.recycler.recipesrecyclerview;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "me.mahakagg.recipesrecyclerview";
+  public static final String APPLICATION_ID = "me.recycler.recipesrecyclerview";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
